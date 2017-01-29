d64755ac5c5f4a6646c1a3702b93d16d3a6cd6cf
Mark S. Everitt <mark.s.everitt@gmail.com>
2017-01-29 18:56:50 +0000

    Update runner to use async-await.

--- bin/cli.js
+++ bin/cli.js
@@ -1,7 +1,16 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --harmony-async-await
 
 'use strict';
 
 const blogEngine = require('../');
 
-blogEngine.build();
+async function run() {
+  try {
+    await blogEngine.build();
+  } catch (e) {
+    console.error(e.stack); // eslint-disable-line no-console
+    process.exit(1);
+  }
+}
+
+run();
