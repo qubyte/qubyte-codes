f68899dfb4cdd4cdd37cc8e5a1510d6fbe3a92e6
Mark S. Everitt <mark.s.everitt@gmail.com>
2017-04-28 18:11:53 +0100

    Makes external links safer.

--- index.js
+++ index.js
@@ -1,57 +1,21 @@
 'use strict';
 
-const marked = require('marked');
-const highlight = require('highlight.js');
 const frontMatter = require('front-matter');
-const fs = require('fs');
 const path = require('path');
 const crypto = require('crypto');
 const handlebars = require('handlebars');
 const slug = require('slug');
 const remark = require('remark');
-
-marked.setOptions({
-  highlight(code, language) {
-    return highlight.highlight(language, code).value;
-  }
-});
+const render = require('./lib/render');
+const baseUrl = require('./lib/baseUrl');
+const fs = require('./lib/fs');
 
 function buildPublicPath(...parts) {
   return path.join(__dirname, 'public', ...parts);
 }
 
-function readDir(dir) {
-  return new Promise((resolve, reject) => {
-    fs.readdir(dir, (err, files) => {
-      if (err) {
-        return reject(err);
-      }
-
-      resolve(files.map(file => path.join(dir, file)));
-    });
-  });
-}
-
-function readFile(file) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(file, 'utf8', (err, content) => err ? reject(err) : resolve(content));
-  });
-}
-
-function writeFile(path, content) {
-  return new Promise((resolve, reject) => {
-    fs.writeFile(path, content, err => err ? reject(err) : resolve());
-  });
-}
-
-function rename(oldPath, newPath) {
-  return new Promise((resolve, reject) => {
-    fs.rename(oldPath, newPath, err => err ? reject(err) : resolve());
-  });
-}
-
 async function hashAndMoveCss() {
-  const content = await readFile(buildPublicPath('main.css'));
+  const content = await fs.readFile(buildPublicPath('main.css'));
 
   const hash = crypto
     .createHash('md5')
@@ -61,14 +25,13 @@ async function hashAndMoveCss() {
   const oldPath = buildPublicPath('main.css');
   const newPath = buildPublicPath(`main-${hash}.css`);
 
-  await rename(oldPath, newPath);
+  await fs.rename(oldPath, newPath);
 
   return `main-${hash}.css`;
 }
 
-
 async function loadTemplate(filename) {
-  const source = await readFile(path.join(__dirname, 'frontend', filename));
+  const source = await fs.readFile(path.join(__dirname, 'frontend', filename));
 
   return handlebars.compile(source);
 }
@@ -96,19 +59,19 @@ function renderMarkdown(post) {
   digested.attributes.updated = new Date(digested.attributes.updated || digested.attributes.datetime);
   digested.attributes.slug = `${slug(digested.attributes.title, { lower: true })}`;
   digested.attributes.filename = `${digested.attributes.slug}.html`;
-  digested.attributes.snippet = marked(makeSnippet(digested.body));
+  digested.attributes.snippet = render(makeSnippet(digested.body));
   digested.attributes.humandatetime = digested.attributes.date.toDateString();
   digested.attributes.isoDate = dateToIso(digested.attributes.date);
   digested.attributes.isoUpdated = dateToIso(digested.attributes.updated);
   digested.attributes.tweetText = encodeURIComponent(`Qubyte Codes - ${digested.attributes.title}`);
-  digested.attributes.canonical = encodeURIComponent(`https://qubyte.codes/blog/${digested.attributes.slug}`);
-  digested.content = marked(digested.body);
+  digested.attributes.canonical = encodeURIComponent(`${baseUrl}/${digested.attributes.slug}`);
+  digested.content = render(digested.body);
   return digested;
 }
 
 async function loadPostFiles() {
-  const files = await readDir(path.join(__dirname, 'posts'));
-  const contents = await Promise.all(files.map(readFile));
+  const files = await fs.readDir(path.join(__dirname, 'posts'));
+  const contents = await Promise.all(files.map(fs.readFile));
 
   return contents.map(renderMarkdown);
 }
@@ -139,10 +102,10 @@ exports.build = async function build() {
   const sitemapTxt = sitemapTemplate({ posts });
 
   await Promise.all([
-    writeFile(buildPublicPath('index.html'), indexHtml),
-    writeFile(buildPublicPath('about.html'), aboutHtml),
-    ...posts.map(post => writeFile(buildPublicPath('blog', post.attributes.filename), post.html)),
-    writeFile(buildPublicPath('atom.xml'), atomXML),
-    writeFile(buildPublicPath('sitemap.txt'), sitemapTxt)
+    fs.writeFile(buildPublicPath('index.html'), indexHtml),
+    fs.writeFile(buildPublicPath('about.html'), aboutHtml),
+    ...posts.map(post => fs.writeFile(buildPublicPath('blog', post.attributes.filename), post.html)),
+    fs.writeFile(buildPublicPath('atom.xml'), atomXML),
+    fs.writeFile(buildPublicPath('sitemap.txt'), sitemapTxt)
   ]);
 };
