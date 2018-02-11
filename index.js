2dae64fb39f7aea7f0a5ffcda693ca1a9f2608f1
Mark S. Everitt <mark.s.everitt@gmail.com>
2018-02-11 22:20:14 +0000

    Adds prev and next rel links.

--- index.js
+++ index.js
@@ -97,6 +97,7 @@ async function renderMarkdown(post, baseUrl) {
   digested.content = await render(digested.body);
   digested.isBlogEntry = true;
   digested.title = `Qubyte Codes - ${title}`;
+  digested.date = new Date(digested.attributes.datetime);
   return digested;
 }
 
@@ -171,10 +172,44 @@ async function getLastPostCommit() {
   return new Date(parseInt(stdout.trim(), 10) * 1000);
 }
 
-exports.build = async function build(baseUrl) {
+async function copyFiles() {
   await createDirectories();
   await cpy(buildSrcPath('icons', '*.png'), buildPublicPath('icons'));
   await cpy(['google*', 'keybase.txt', 'index.js', 'sw.js'].map(n => buildSrcPath(n)), buildPublicPath());
+}
+
+function dateDescending(a, b) {
+  return b.date - a.date;
+}
+
+function renderPosts(posts, blogTemplate, cssPath, dev) {
+  const rendered = [];
+
+  for (let i = 0; i < posts.length; i++) {
+    const previous = posts[i + 1];
+    const post = posts[i];
+    const next = posts[i - 1];
+    const renderObject = { ...post, cssPath, dev };
+
+    if (previous) {
+      renderObject.prevLink = `/blog/${previous.attributes.slug}`;
+    }
+
+    if (next) {
+      renderObject.nextLink = `/blog/${next.attributes.slug}`;
+    }
+
+    rendered.push({
+      html: blogTemplate(renderObject),
+      filename: post.attributes.filename
+    });
+  }
+
+  return rendered;
+}
+
+exports.build = async function build(baseUrl) {
+  await copyFiles();
 
   const {
     indexTemplate,
@@ -186,16 +221,12 @@ exports.build = async function build(baseUrl) {
     sitemapTemplate
   } = await loadTemplates();
 
-  const posts = (await loadPostFiles(baseUrl)).sort((a, b) => new Date(b.attributes.datetime) - new Date(a.attributes.datetime));
+  const posts = (await loadPostFiles(baseUrl)).sort(dateDescending);
   const cssPath = await generateCss(path.join(__dirname, 'src', 'css', 'entry.css'));
   const tags = collateTags(posts);
   const dev = process.env.NODE_ENV === 'development';
 
-  for (const post of posts) {
-    post.dev = dev;
-    post.cssPath = cssPath;
-    post.html = blogTemplate(post);
-  }
+  const renderedPosts = renderPosts(posts, blogTemplate, cssPath, dev);
 
   const indexHtml = indexTemplate({ posts, cssPath, dev, title: 'Qubyte Codes' });
   const aboutHtml = aboutTemplate({ cssPath, dev, title: 'Qubyte Codes - about' });
@@ -207,7 +238,7 @@ exports.build = async function build(baseUrl) {
     writeFile(buildPublicPath('index.html'), indexHtml),
     writeFile(buildPublicPath('about.html'), aboutHtml),
     writeFile(buildPublicPath('webmention.html'), webmentionHtml),
-    ...posts.map(post => writeFile(buildPublicPath('blog', post.attributes.filename), post.html)),
+    ...renderedPosts.map(({ html, filename }) => writeFile(buildPublicPath('blog', filename), html)),
     ...Object.entries(tags).map(([tag, posts]) => {
       const tagHtml = tagTemplate({ posts, tag, cssPath, dev, title: `Qubyte Codes - Posts tagged as ${tag}` });
 
