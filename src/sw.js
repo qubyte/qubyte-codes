645bd5317930e967ebd18a759d0283f0cb632bd5
Mark S. Everitt <mark.s.everitt@gmail.com>
2018-04-23 23:18:32 +0100

    Removes dead lines from the service worker, and update nginx config.

--- src/sw.js
+++ src/sw.js
@@ -16,10 +16,6 @@ addEventListener('fetch', fetchEvent => {
     return;
   }
 
-  // if (request.cache === 'only-if-cache') {
-  //   request.mode = 'same-origin';
-  // }
-
   const responseFromFetch = fetch(request);
   const cachePromise = caches.open(cacheName);
   const clonedResponsePromise = responseFromFetch.then(res => res.clone());
