119ee6ffe7083b97dc0894213828d00108192c35
Mark S. Everitt <mark.s.everitt@gmail.com>
2019-01-24 18:00:13 +0000

    Some error handling and a config request handing added to the micropub function.

--- functions/micropub.js
+++ functions/micropub.js
@@ -1,9 +1,40 @@
 'use strict';
 
 const { request } = require('https');
-const OWNER = 'qubyte';
-const REPO = 'qubyte-codes';
-const PATH = 'src/notes';
+
+function consumeResponse(res) {
+  const chunks = [];
+
+  function onData(d) {
+    chunks.push(d);
+  }
+
+  return new Promise((resolve, reject) => {
+    function onError(error) {
+      res.removeListener('data', onData);
+      res.removeListener('error', onError);
+      res.removeListener('end', onEnd);
+
+      reject(error);
+    }
+
+    function onEnd() {
+      res.removeListener('data', onData);
+      res.removeListener('error', onError);
+      res.removeListener('end', onEnd);
+
+      try {
+        resolve(JSON.parse(Buffer.concat(chunks)));
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    res.on('data', onData);
+    res.on('error', onError);
+    res.on('end', onEnd);
+  });
+}
 
 function checkAuth(Authorization) {
   const options = {
@@ -17,46 +48,53 @@ function checkAuth(Authorization) {
   };
 
   return new Promise((resolve, reject) => {
-    request(options, res => {
-      const chunks = [];
-
-      res.on('data', d => chunks.push(d));
-      res.on('error', reject);
-      res.on('end', () => {
-        let body;
-
-        try {
-          body = JSON.parse(Buffer.concat(chunks));
-        } catch (e) {
-          return reject(e);
-        }
-
-        if (body.me !== 'https://qubyte.codes/') {
-          return reject(new Error('Not authorized.'));
-        }
-
-        if (!(body.scope.includes('create') || body.scope.includes('post'))) {
-          return reject(new Error('Not authorized to post.'));
-        }
-
-        resolve();
-      });
-    })
-      .on('error', reject)
+    const req = request(options)
+      .on('res', onRes)
+      .on('error', onError)
       .end();
+
+    async function onRes(res) {
+      req.removeListener('res', onRes);
+      req.removeListener('error', onError);
+
+      let body;
+
+      try {
+        body = await consumeResponse(res);
+      } catch (e) {
+        return reject(e);
+      }
+
+      if (body.me !== 'https://qubyte.codes/') {
+        return reject(new Error('Not authorized.'));
+      }
+
+      if (!(body.scope.includes('create') || body.scope.includes('post'))) {
+        return reject(new Error('Not authorized to post.'));
+      }
+
+      resolve();
+    }
+
+    function onError(error) {
+      req.removeListener('res', onRes);
+      req.removeListener('error', onError);
+
+      reject(error);
+    }
   });
 }
 
 function createFile(message, content) {
-  const body = JSON.stringify({ message, content }); // TODO check base64 content
+  const body = JSON.stringify({ message, content });
 
   console.log('CREATING FILE:', body); // eslint-disable-line
 
   const options = {
     method: 'PUT',
     host: 'api.github.com',
-    path: `/repos/${OWNER}/${REPO}/contents/${PATH}/${Date.now()}`,
-    auth: `${OWNER}:${process.env.GITHUB_TOKEN}`,
+    path: `/repos/qubyte/qubyte-codes/contents/src/notes/${Date.now()}`,
+    auth: `qubyte:${process.env.GITHUB_TOKEN}`,
     headers: {
       'Content-Type': 'application/json',
       'Content-Length': Buffer.byteLength(body),
@@ -65,35 +103,94 @@ function createFile(message, content) {
   };
 
   return new Promise((resolve, reject) => {
-    request(options, res => {
+    const req = request(options)
+      .on('res', onRes)
+      .on('error', onError)
+      .end(body);
+
+    async function onRes(res) {
+      req.removeListener('res', onRes);
+      req.removeListener('error', onError);
+
+      let body;
+
+      try {
+        body = await consumeResponse(res);
+      } catch (e) {
+        return reject(e);
+      }
+
       if (res.statusCode === 201) {
-        res.resume();
         return resolve();
       }
 
-      const chunks = [];
+      console.log(body); // eslint-disable-line
 
-      res.on('data', d => chunks.push(d));
-      res.on('end', () => {
-        console.log(Buffer.concat(chunks).toString()); // eslint-disable-line
-        reject(new Error(`Unexpected status from GitHub ${res.statusCode}.`));
-      });
-    })
-      .on('error', reject)
-      .end(body);
+      reject(new Error(`Unexpected status from GitHub ${res.statusCode}.`));
+    }
+
+    function onError(error) {
+      req.removeListener('res', onRes);
+      req.removeListener('error', onError);
+
+      reject(error);
+    }
   });
 }
 
-exports.handler = async function (event) {
+exports.handler = async function (event, context, callback) {
   console.log('EVENT', event); // eslint-disable-line
 
   await checkAuth(event.headers.authorization);
 
-  const encoded = event.isBase64Encoded ? event.body : Buffer.from(event.body).toString('base64');
+  const syndications = [
+    {
+      uid: 'https://twitter.com/qubyte',
+      name: 'qubyte on twitter',
+      service: {
+        name: 'Twitter',
+        url: 'https://twitter.com/',
+        photo: 'https://abs.twimg.com/icons/apple-touch-icon-192x192.png'
+      },
+      user: {
+        name: 'qubyte',
+        url: 'https://twitter.com/qubyte'
+      }
+    },
+    {
+      uid: 'https://mastodon.social/@qubyte',
+      name: 'qubyte on mastodon.social',
+      service: {
+        name: 'Mastodon',
+        url: 'https://mastodon.social/'
+      },
+      user: {
+        name: '@qubyte',
+        url: 'https://mastodon.social/@qubyte',
+        photo: 'https://files.mastodon.social/accounts/avatars/000/034/232/original/19ce997f84ca75fe.png'
+      }
+    }
+  ];
 
-  if (!encoded.length) {
+  if (event.queryStringParameters.q === 'syndicate-to') {
+    return callback(null, {
+      'syndicate-to': syndications
+    });
+  }
+
+  if (event.queryStringParameters.q === 'config') {
+    return callback(null, {
+      'syndicate-to': syndications
+    });
+  }
+
+  if (!event.body) {
     return;
   }
 
+  const encoded = event.isBase64Encoded ? event.body : Buffer.from(event.body).toString('base64');
+
   await createFile('New note.', encoded);
+
+  callback()
 };
