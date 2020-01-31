c212ce210cdaeabeda3dea162a4308d6b9c8a3f5
Mark S. Everitt <mark.s.everitt@gmail.com>
2020-01-31 17:25:52 +0000

    Adds a graph-based build system.

--- index.js
+++ index.js
@@ -10,41 +10,12 @@ const loadNoteFiles = require('./lib/load-note-files');
 const loadLinkFiles = require('./lib/load-link-files');
 const loadLikeFiles = require('./lib/load-like-files');
 const loadReplyFiles = require('./lib/load-reply-files');
-const appendPostContent = require('./lib/append-post-content');
 const collateTags = require('./lib/collate-tags');
 const getLastCommitTime = require('./lib/get-last-commit-time');
+const ExecutionGraph = require('./lib/execution-graph');
 const fs = require('fs').promises;
 const cpy = require('cpy');
 const publications = require('./src/publications');
-
-// Copies static files and directories to a fresh public directory.
-async function copyFiles(source, target, content) {
-  function copy(directory, subDirectory) {
-    return cpy(path.join(directory, subDirectory, '*'), path.join(target, subDirectory));
-  }
-
-  await fs.mkdir(target);
-
-  await Promise.all([
-    fs.mkdir(path.join(target, 'blog')),
-    fs.mkdir(path.join(target, 'notes')),
-    fs.mkdir(path.join(target, 'links')),
-    fs.mkdir(path.join(target, 'likes')),
-    fs.mkdir(path.join(target, 'replies')),
-    fs.mkdir(path.join(target, 'tags')),
-    fs.mkdir(path.join(target, 'images')),
-    copy(source, 'icons'),
-    copy(source, 'fonts'),
-    copy(source, 'img'),
-    copy(content, 'scripts'),
-    copy(content, 'papers'),
-    copy(content, 'images'),
-    cpy(
-      ['google*', 'keybase.txt', 'robots.txt', 'index.js', 'sw.js', 'manifest.json'].map(n => path.join(source, n)),
-      target
-    )
-  ]);
-}
 
 async function writePublicFile(content, ...pathParts) {
   await fs.writeFile(path.join(...pathParts), content);
@@ -53,75 +24,342 @@ async function writePublicFile(content, ...pathParts) {
 // This is where it all kicks off. This function loads posts and templates,
 // renders it all to files, and saves them to the public directory.
 
-exports.build = async function build({ baseUrl, baseTitle, dev, syndications }) {
+exports.build = function build({ baseUrl, baseTitle, dev, syndications }) {
   const source = path.join(__dirname, 'src');
   const target = path.join(__dirname, 'public');
   const content = path.join(__dirname, 'content');
 
-  const gettingLastContentCommitTime = getLastCommitTime(path.join(content));
+  const graph = new ExecutionGraph();
 
-  const [templates, posts, notes, links, likes, replies, cssPath] = await Promise.all([
-    // Load and compile markdown template files into functions.
-    loadTemplates(path.join(source, 'templates'), { baseTitle }),
-    // Load markdown posts, render them to HTML content, and sort them by date descending.
-    loadPostFiles(path.join(content, 'posts'))
-      .then(posts => Promise.all(posts.map(p => appendPostContent(p, baseUrl)))),
-    // Load short form notes, and reposts (links), render them to HTML content, and sort them by date descending.
-    loadNoteFiles(path.join(content, 'notes'), syndications),
-    loadLinkFiles(path.join(content, 'links'), syndications),
-    loadLikeFiles(path.join(content, 'likes')),
-    loadReplyFiles(path.join(content, 'replies')),
-    // After creating the target directory structure, compile CSS to a single file, with a unique filename.
-    copyFiles(source, target, content)
-      .then(() => generateCss(path.join(source, 'css'), target, 'entry.css', 'default'))
-  ]);
+  async function makeDirectory(...pathParts) {
+    const directory = path.join(target, ...pathParts);
 
-  // Make a list of tags found in posts.
-  const tags = collateTags(posts, cssPath, dev, templates.tag);
+    await fs.rmdir(directory, { recursive: true });
+    await fs.mkdir(directory);
+  }
 
-  function renderResources(resources, template) {
+  function renderResources(resources, template, cssPath) {
     return resources.map(resource => ({ html: template({ ...resource, cssPath, dev }), filename: resource.filename }));
   }
 
-  // Render various pages.
-  const renderedBlogs = renderResources(posts, templates.blog);
-  const renderedNotes = renderResources(notes, templates.note);
-  const renderedLinks = renderResources(links, templates.link);
-  const renderedLikes = renderResources(likes, templates.like);
-  const renderedReplies = renderResources(replies, templates.reply);
-  const blogsHtml = templates.blogs({ posts, cssPath, dev, localUrl: '/blog', title: 'Archive' });
-  const notesHtml = templates.notes({ notes, cssPath, dev, localUrl: '/notes', title: 'Notes' });
-  const linksHtml = templates.links({ links, cssPath, dev, localUrl: '/links', title: 'Links' });
-  const likesHtml = templates.likes({ likes, cssPath, dev, localUrl: '/likes', title: 'Likes' });
-  const repliesHtml = templates.replies({ replies, cssPath, dev, localUrl: '/replies', title: 'Replies' });
-  const aboutHtml = templates.about({ cssPath, dev, localUrl: '/', title: 'About' });
-  const publicationsHtml = templates.publications({ cssPath, dev, localUrl: '/publications', publications, title: 'Publications' });
-  const fourOhFourHtml = templates[404]({ cssPath, dev, localUrl: '/404', title: 'Not Found' });
-  const webmentionHtml = templates.webmention({ cssPath, dev, localUrl: '/webmention', title: 'Webmention' });
+  return graph.addNodes({
+    commitTime: {
+      action() {
+        return getLastCommitTime(content);
+      }
+    },
+    templates: {
+      action() {
+        return loadTemplates(path.join(source, 'templates'), { baseTitle });
+      }
+    },
+    postFiles: {
+      action() {
+        return loadPostFiles(path.join(content, 'posts'), baseUrl);
+      }
+    },
+    noteFiles: {
+      action() {
+        return loadNoteFiles(path.join(content, 'notes'), syndications);
+      }
+    },
+    linkFiles: {
+      action() {
+        return loadLinkFiles(path.join(content, 'links'), syndications);
+      }
+    },
+    likeFiles: {
+      action() {
+        return loadLikeFiles(path.join(content, 'likes'));
+      }
+    },
+    replyFiles: {
+      action() {
+        return loadReplyFiles(path.join(content, 'replies'));
+      }
+    },
+    publicDirectory: {
+      action() {
+        return makeDirectory();
+      }
+    },
+    blogDirectory: {
+      dependencies: ['publicDirectory'],
+      action() {
+        return makeDirectory('blog');
+      }
+    },
+    notesDirectory: {
+      dependencies: ['publicDirectory'],
+      action() {
+        return makeDirectory('notes');
+      }
+    },
+    linksDirectory: {
+      dependencies: ['publicDirectory'],
+      action() {
+        return makeDirectory('links');
+      }
+    },
+    likesDirectory: {
+      dependencies: ['publicDirectory'],
+      action() {
+        return makeDirectory('likes');
+      }
+    },
+    repliesDirectory: {
+      dependencies: ['publicDirectory'],
+      action() {
+        return makeDirectory('replies');
+      }
+    },
+    tagsDirectory: {
+      dependencies: ['publicDirectory'],
+      action() {
+        return makeDirectory('tags');
+      }
+    },
+    staticFiles: {
+      dependencies: ['publicDirectory'],
+      action() {
+        function copy(directory, subDirectory) {
+          return cpy(path.join(directory, subDirectory, '*'), path.join(target, subDirectory));
+        }
 
-  // Render the site map.
-  const sitemapTxt = templates.sitemap({ posts, tags, notes, links, likes, replies });
-  const everything = [...posts, ...notes, ...links, ...likes, ...replies].sort((a, b) => b.timestamp - a.timestamp);
+        return Promise.all([
+          copy(source, 'icons'),
+          copy(source, 'fonts'),
+          copy(source, 'img'),
+          copy(content, 'scripts'),
+          copy(content, 'papers'),
+          copy(content, 'images'),
+          cpy(
+            ['google*', 'keybase.txt', 'robots.txt', 'index.js', 'sw.js', 'manifest.json'].map(n => path.join(source, n)),
+            target
+          )
+        ]);
+      }
+    },
+    css: {
+      dependencies: ['publicDirectory'],
+      action() {
+        return generateCss(path.join(source, 'css'), target, 'entry.css', 'default');
+      }
+    },
+    collatedTags: {
+      dependencies: ['css', 'templates', 'postFiles'],
+      action({ postFiles, css, templates }) {
+        return collateTags(postFiles, css, dev, templates.tag);
+      }
+    },
+    renderedPosts: {
+      dependencies: ['css', 'templates', 'postFiles'],
+      action({ postFiles, templates, css }) {
+        return renderResources(postFiles, templates.blog, css);
+      }
+    },
+    renderedNotes: {
+      dependencies: ['css', 'templates', 'noteFiles'],
+      action({ noteFiles, templates, css }) {
+        return renderResources(noteFiles, templates.note, css);
+      }
+    },
+    renderedLinks: {
+      dependencies: ['css', 'templates', 'linkFiles'],
+      action({ linkFiles, templates, css }) {
+        return renderResources(linkFiles, templates.link, css);
+      }
+    },
+    renderedLikes: {
+      dependencies: ['css', 'templates', 'likeFiles'],
+      action({ likeFiles, templates, css }) {
+        return renderResources(likeFiles, templates.like, css);
+      }
+    },
+    renderedReplies: {
+      dependencies: ['css', 'templates', 'replyFiles'],
+      action({ replyFiles, templates, css }) {
+        return renderResources(replyFiles, templates.replies, css);
+      }
+    },
+    renderedBlogIndex: {
+      dependencies: ['css', 'templates', 'postFiles'],
+      action({ postFiles: posts, templates, css: cssPath }) {
+        return templates.blogs({ posts, cssPath, dev, localUrl: '/blog', title: 'Archive' });
+      }
+    },
+    renderedNotesIndex: {
+      dependencies: ['css', 'templates', 'noteFiles'],
+      action({ noteFiles: notes, templates, css: cssPath }) {
+        return templates.notes({ notes, cssPath, dev, localUrl: '/notes', title: 'Notes' });
+      }
+    },
+    renderedLinksIndex: {
+      dependencies: ['css', 'templates', 'linkFiles'],
+      action({ linkFiles: links, templates, css: cssPath }) {
+        return templates.links({ links, cssPath, dev, localUrl: '/links', title: 'Links' });
+      }
+    },
+    renderedLikesIndex: {
+      dependencies: ['css', 'templates', 'likeFiles'],
+      action({ likeFiles: likes, templates, css: cssPath }) {
+        return templates.likes({ likes, cssPath, dev, localUrl: '/likes', title: 'Likes' });
+      }
+    },
+    renderedRepliesIndex: {
+      dependencies: ['css', 'templates', 'replyFiles'],
+      action({ replyFiles: replies, templates, css: cssPath }) {
+        return templates.replies({ replies, cssPath, dev, localUrl: '/replies', title: 'Replies' });
+      }
+    },
+    renderedAbout: {
+      dependencies: ['css', 'templates'],
+      action({ templates, css: cssPath }) {
+        return templates.about({ cssPath, dev, localUrl: '/', title: 'About' });
+      }
+    },
+    renderedPublications: {
+      dependencies: ['css', 'templates'],
+      action({ templates, css: cssPath }) {
+        return templates.publications({ cssPath, dev, localUrl: '/publications', publications, title: 'Publications' });
+      }
+    },
+    renderedFourOhFour: {
+      dependencies: ['css', 'templates'],
+      action({ templates, css: cssPath }) {
+        return templates[404]({ cssPath, dev, localUrl: '/404', title: 'Not Found' });
+      }
+    },
+    renderedWebmentionConfirmation: {
+      dependencies: ['css', 'templates'],
+      action({ templates, css: cssPath }) {
+        return templates.webmention({ cssPath, dev, localUrl: '/webmention', title: 'Webmention' });
+      }
+    },
+    renderedSitemap: {
+      dependencies: ['templates', 'collatedTags', 'postFiles', 'noteFiles', 'linkFiles', 'likeFiles', 'replyFiles'],
+      action({
+        templates,
+        collatedTags: tags,
+        postFiles: posts,
+        noteFiles: notes,
+        linkFiles: links,
+        likeFiles: likes,
+        replyFiles: replies
+      }) {
+        return templates.sitemap({ posts, tags, notes, links, likes, replies });
+      }
+    },
+    renderedAtomFeed: {
+      dependencies: ['templates', 'commitTime', 'postFiles', 'noteFiles', 'linkFiles', 'likeFiles', 'replyFiles'],
+      action({ templates, commitTime, postFiles, noteFiles, linkFiles, likeFiles, replyFiles }) {
+        const items = [...postFiles, ...noteFiles, ...linkFiles, ...likeFiles, ...replyFiles]
+          .sort((a, b) => b.timestamp - a.timestamp);
 
-  // Write the rendered templates to the public directory.
-  await Promise.all([
-    writePublicFile(aboutHtml, target, 'index.html'),
-    writePublicFile(blogsHtml, target, 'blog', 'index.html'),
-    writePublicFile(notesHtml, target, 'notes', 'index.html'),
-    writePublicFile(linksHtml, target, 'links', 'index.html'),
-    writePublicFile(likesHtml, target, 'likes', 'index.html'),
-    writePublicFile(repliesHtml, target, 'replies', 'index.html'),
-    writePublicFile(publicationsHtml, target, 'publications.html'),
-    writePublicFile(webmentionHtml, target, 'webmention.html'),
-    writePublicFile(fourOhFourHtml, target, '404.html'),
-    ...renderedBlogs.map(post => writePublicFile(post.html, target, 'blog', post.filename)),
-    ...renderedNotes.map(note => writePublicFile(note.html, target, 'notes', note.filename)),
-    ...renderedLinks.map(link => writePublicFile(link.html, target, 'links', link.filename)),
-    ...renderedLikes.map(like => writePublicFile(like.html, target, 'likes', like.filename)),
-    ...renderedReplies.map(reply => writePublicFile(reply.html, target, 'replies', reply.filename)),
-    ...tags.map(tag => writePublicFile(tag.rendered, target, 'tags', tag.filename)),
-    writePublicFile(sitemapTxt, target, 'sitemap.txt'),
-    gettingLastContentCommitTime
-      .then(updated => writePublicFile(templates.atom({ items: everything, updated }), target, 'atom.xml'))
-  ]);
+        return templates.atom({ items, updated: commitTime });
+      }
+    },
+    writtenIndex: {
+      dependencies: ['renderedAbout'],
+      action({ renderedAbout }) {
+        writePublicFile(renderedAbout, target, 'index.html');
+      }
+    },
+    writtenBlogIndex: {
+      dependencies: ['renderedBlogIndex'],
+      action({ renderedBlogIndex }) {
+        return writePublicFile(renderedBlogIndex, target, 'blog', 'index.html');
+      }
+    },
+    writtenNotesIndex: {
+      dependencies: ['renderedNotesIndex'],
+      action({ renderedNotesIndex }) {
+        return writePublicFile(renderedNotesIndex, target, 'notes', 'index.html');
+      }
+    },
+    writtenLinksIndex: {
+      dependencies: ['renderedLinksIndex'],
+      action({ renderedLinksIndex }) {
+        return writePublicFile(renderedLinksIndex, target, 'links', 'index.html');
+      }
+    },
+    writtenLikesIndex: {
+      dependencies: ['renderedLikesIndex'],
+      action({ renderedLikesIndex }) {
+        return writePublicFile(renderedLikesIndex, target, 'likes', 'index.html');
+      }
+    },
+    writtenRepliesIndex: {
+      dependencies: ['renderedRepliesIndex'],
+      action({ renderedRepliesIndex }) {
+        return writePublicFile(renderedRepliesIndex, target, 'replies', 'index.html');
+      }
+    },
+    writtenPublications: {
+      dependencies: ['renderedPublications'],
+      action({ renderedPublications }) {
+        return writePublicFile(renderedPublications, target, 'publications.html');
+      }
+    },
+    writtenWebmentionConfirmation: {
+      dependencies: ['renderedWebmentionConfirmation'],
+      action({ renderedWebmentionConfirmation }) {
+        return writePublicFile(renderedWebmentionConfirmation, target, 'webmention.html');
+      }
+    },
+    writtenFourOhFour: {
+      dependencies: ['renderedFourOhFour'],
+      action({ renderedFourOhFour }) {
+        return writePublicFile(renderedFourOhFour, target, '404.html');
+      }
+    },
+    writtenSitemap: {
+      dependencies: ['renderedSitemap'],
+      action({ renderedSitemap }) {
+        return writePublicFile(renderedSitemap, target, 'sitemap.txt');
+      }
+    },
+    writtenAtomFeed: {
+      dependencies: ['renderedAtomFeed'],
+      action({ renderedAtomFeed }) {
+        return writePublicFile(renderedAtomFeed, target, 'atom.xml');
+      }
+    },
+    writtenPosts: {
+      dependencies: ['renderedPosts'],
+      action({ renderedPosts }) {
+        return renderedPosts.map(post => writePublicFile(post.html, target, 'blog', post.filename));
+      }
+    },
+    writtenNotes: {
+      dependencies: ['renderedNotes'],
+      action({ renderedNotes }) {
+        return renderedNotes.map(note => writePublicFile(note.html, target, 'notes', note.filename));
+      }
+    },
+    writtenLinks: {
+      dependencies: ['renderedLinks'],
+      action({ renderedLinks }) {
+        return renderedLinks.map(note => writePublicFile(note.html, target, 'links', note.filename));
+      }
+    },
+    writtenLikes: {
+      dependencies: ['renderedLikes'],
+      action({ renderedLikes }) {
+        return renderedLikes.map(note => writePublicFile(note.html, target, 'likes', note.filename));
+      }
+    },
+    writtenReplies: {
+      dependencies: ['renderedReplies'],
+      action({ renderedReplies }) {
+        return renderedReplies.map(note => writePublicFile(note.html, target, 'replies', note.filename));
+      }
+    },
+    writtenTags: {
+      dependencies: ['collatedTags'],
+      action({ collatedTags }) {
+        return collatedTags.map(note => writePublicFile(note.html, target, 'tags', note.filename));
+      }
+    }
+  });
 };
---
+++ tests/units/execution-graph.test.js
@@ -0,0 +1,271 @@
+'use strict';
+
+const assert = require('assert').strict;
+const ExecutionGraph = require('../../lib/execution-graph');
+const { EventEmitter } = require('events');
+const crypto = require('crypto');
+const wait = require('util').promisify(setTimeout);
+
+describe.only('execution-graph', () => {
+  let graph;
+
+  beforeEach(() => {
+    graph = new ExecutionGraph();
+  });
+
+  it('is a function', () => {
+    assert.equal(typeof ExecutionGraph, 'function');
+  });
+
+  it('creates execution graph instances', () => {
+    assert.ok(graph instanceof ExecutionGraph);
+  });
+
+  it('graphs are instances of EventEmitter', () => {
+    assert.ok(graph instanceof EventEmitter);
+  });
+
+  describe('adding nodes', () => {
+    it('returns a promise', () => {
+      assert.ok(graph.addNode({ name: 'a-name', action() {} }) instanceof Promise);
+    });
+
+    it('resolves the promise after a synchronous action is run when there are no dependencies', async () => {
+      const order = [];
+
+      graph.once('done:a-name', () => order.push('event'));
+
+      const promise = graph.addNode({
+        name: 'a-name',
+        action() {
+          order.push('action');
+        }
+      });
+
+      await promise.then(() => order.push('promise'));
+
+      assert.deepEqual(order, ['action', 'event', 'promise']);
+    });
+
+    it('resolves to the return value of a synchronous action to the node', async () => {
+      const expected = crypto.randomBytes(8).toString('hex');
+      const result = await graph.addNode({
+        name: 'a-name',
+        action() {
+          return expected;
+        }
+      });
+
+      assert.equal(result, expected);
+    });
+
+    it('resolves the promise after an asynchronous action is run when there are no dependencies', async () => {
+      const order = [];
+
+      graph.once('done:a-name', () => order.push('event'));
+
+      const promise = graph.addNode({
+        name: 'a-name',
+        async action() {
+          await wait(100);
+          order.push('action');
+        }
+      });
+
+      await promise.then(() => order.push('promise'));
+
+      assert.deepEqual(order, ['action', 'event', 'promise']);
+    });
+
+    it('resolves to the resulution value of an asynchronous action to the node', async () => {
+      const expected = crypto.randomBytes(8).toString('hex');
+      const result = await graph.addNode({
+        name: 'a-name',
+        action() {
+          return Promise.resolve(expected);
+        }
+      });
+
+      assert.equal(result, expected);
+    });
+
+    it('waits for dependencies which are not yet registered before resolving', async () => {
+      const order = [];
+      const promises = [];
+
+      promises.push(graph.addNode({
+        name: 'a',
+        dependencies: ['b', 'c'],
+        action() {
+          assert.deepEqual(order, ['c', 'b']);
+        }
+      }).then(() => order.push('a')));
+
+      promises.push(graph.addNode({
+        name: 'b',
+        action() {
+          return wait(100);
+        }
+      }).then(() => order.push('b')));
+
+      promises.push(graph.addNode({
+        name: 'c',
+        action() {
+          return wait(50);
+        }
+      }).then(() => order.push('c')));
+
+      await Promise.all(promises);
+
+      assert.deepEqual(order, ['c', 'b', 'a']);
+    });
+
+    it('passes results from dependencies into an action', async () => {
+      graph.addNode({
+        name: 'a',
+        action() {
+          return 'result-a';
+        }
+      });
+
+      graph.addNode({
+        name: 'b',
+        action() {
+          return Promise.resolve('result-b');
+        }
+      });
+
+      graph.addNode({
+        name: 'c',
+        async action() {
+          await wait(10);
+          return 'result-c';
+        }
+      });
+
+      const passedIn = await graph.addNode({
+        name: 'd',
+        dependencies: ['a', 'b', 'c'],
+        action(input) {
+          return input;
+        }
+      });
+
+      assert.deepEqual(passedIn, { a: 'result-a', b: 'result-b', c: 'result-c' });
+    });
+  });
+
+  describe('removing nodes', () => {
+    it('removes a node with no dependencies', async () => {
+      await graph.addNode({
+        name: 'a',
+        action() {}
+      });
+
+      graph.removeNode({ name: 'a' });
+
+      assert.equal(graph.nodes.size, 0);
+    });
+
+    it('throws when attempting to remove a node with dependencies', async () => {
+      await Promise.all([
+        graph.addNode({
+          name: 'a',
+          action() {}
+        }),
+        graph.addNode({
+          name: 'b',
+          dependencies: ['a'],
+          action() {}
+        }),
+        graph.addNode({
+          name: 'c',
+          dependencies: ['a'],
+          action() {}
+        })
+      ]);
+
+      assert.throws(
+        () => graph.removeNode({ name: 'a' }),
+        Error,
+        'Node a has dependent nodes: b, c'
+      );
+
+      assert.deepEqual([...graph.nodes.keys()], ['a', 'b', 'c']);
+    });
+  });
+
+  describe('rerunning nodes', () => {
+    let nodesRerun;
+
+    beforeEach(async () => {
+      nodesRerun = [];
+
+      await Promise.all([
+        graph.addNode({
+          name: 'a',
+          async action() {
+            await wait(10);
+            nodesRerun.push('a');
+          }
+        }),
+        graph.addNode({
+          name: 'b',
+          dependencies: ['a'],
+          async action() {
+            await wait(10);
+            nodesRerun.push('b');
+          }
+        }),
+        graph.addNode({
+          name: 'c',
+          dependencies: ['b'],
+          async action() {
+            await wait(10);
+            nodesRerun.push('c');
+          }
+        })
+      ]);
+
+      nodesRerun = [];
+    });
+
+    it('reruns leaf nodes', async () => {
+      await graph.rerunNode({ name: 'c' });
+
+      assert.deepEqual(nodesRerun, ['c']);
+    });
+
+    it('reruns on-leaf nodes and their decendents in order', async () => {
+      await graph.rerunNode({ name: 'b' });
+
+      assert.deepEqual(nodesRerun, ['b', 'c']);
+    });
+  });
+
+  describe('getting results', () => {
+    it('gets results of returned and resolved nodes', async () => {
+      graph.addNode({
+        name: 'a',
+        action() {
+          return 1;
+        }
+      });
+      graph.addNode({
+        name: 'c',
+        async action() {
+          await wait(200);
+          return '3';
+        }
+      });
+      await graph.addNode({
+        name: 'b',
+        action() {
+          return Promise.resolve(2);
+        }
+      });
+
+      assert.deepEqual(graph.results, { a: 1, b: 2 });
+    });
+  });
+});
