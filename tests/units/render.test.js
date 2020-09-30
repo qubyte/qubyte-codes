ab7de4c0a0cc7c132d96735e955429364d11be77
Mark S. Everitt <mark.s.everitt@gmail.com>
2020-09-30 13:58:12 +0100

    Update mathematics handling to avoid obviously abusing codeblocks, and allow more recognisable maths blocks.

--- tests/units/render.test.js
+++ tests/units/render.test.js
@@ -10,20 +10,14 @@ describe('render', () => {
     assert.equal(typeof render, 'function');
   });
 
-  it('returns a promise', () => {
-    const result = render('', URL);
-
-    assert.ok(result instanceof Promise);
-  });
-
-  it('resolves to rendered content', async () => {
-    const rendered = await render('a paragraph', URL);
+  it('returns rendered content', () => {
+    const rendered = render('a paragraph', URL);
 
     assert.equal(rendered.trim(), '<p>a paragraph</p>');
   });
 
-  it('renders fenced blocks labelled as SVG in the img role and a title containing the original maths', async () => {
-    const rendered = await render('```mathematics\na=b\n```', URL);
+  it('renders $$ delimited blocks labelled as SVG in the img role and a title containing the original maths', () => {
+    const rendered = render('\\[\na=b\n\\]', URL);
 
     const { window: { document } } = new JSDOM(rendered);
     const $svg = document.querySelector('svg');
@@ -35,42 +29,83 @@ describe('render', () => {
     assert.equal($title.textContent, 'a=b');
   });
 
-  it('removes inline style from rendered mathematics', async () => {
-    const rendered = await render('```mathematics\na=b\n```', URL);
+  describe('mathematics blocks with brace delimeters', () => {
+    it('removes inline style from rendered mathematics', () => {
+      const rendered = render('\\[\na=b\n\\]', URL);
 
-    const { window: { document } } = new JSDOM(rendered);
+      const { window: { document } } = new JSDOM(rendered);
 
-    assert.equal(document.querySelectorAll('svg[style]').length, 0);
+      assert.equal(document.querySelectorAll('svg[style]').length, 0);
+    });
+
+    it('updates the ID of the title of the rendered mathematics', () => {
+      const rendered = render('\\[\na=b\n\\]', URL);
+
+      const { window: { document } } = new JSDOM(rendered);
+      const titles = document.querySelectorAll('svg title[id]');
+
+      assert.equal(titles.length, 1);
+      assert.equal(titles[0].id, 'equation-1');
+    });
+
+    it('increments the title ID to avoid collisions', () => {
+      const rendered = render('\\[\na=b\n\\]\n\ntext\n\n\\[\nc=d\n\\]', URL);
+      const { window: { document } } = new JSDOM(rendered);
+      const titles = document.querySelectorAll('svg title[id]');
+
+      assert.equal(titles.length, 2);
+      assert.equal(titles[0].id, 'equation-1');
+      assert.equal(titles[1].id, 'equation-2');
+    });
+
+    it('sets aria-labelledby to the title element ID for each snipped', () => {
+      const rendered = render('\\[\na=b\n\\]\n\ntext\n\n\\[\nc=d\n\\]', URL);
+
+      const { window: { document } } = new JSDOM(rendered);
+      const svgs = document.querySelectorAll('svg');
+
+      assert.equal(svgs[0].getAttribute('aria-labelledby'), 'equation-1');
+      assert.equal(svgs[1].getAttribute('aria-labelledby'), 'equation-2');
+    });
   });
 
-  it('updates the ID of the title of the rendered mathematics', async () => {
-    const rendered = await render('```mathematics\na=b\n```', URL);
+  describe('mathematics with dollar delimiters', () => {
+    it('removes inline style from rendered mathematics', () => {
+      const rendered = render('$$\na=b\n$$', URL);
 
-    const { window: { document } } = new JSDOM(rendered);
-    const titles = document.querySelectorAll('svg title[id]');
+      const { window: { document } } = new JSDOM(rendered);
 
-    assert.equal(titles.length, 1);
-    assert.equal(titles[0].id, 'equation-1');
-  });
+      assert.equal(document.querySelectorAll('svg[style]').length, 0);
+    });
 
-  it('increments the title ID to avoid collisions', async () => {
-    const rendered = await render('```mathematics\na=b\n```\ntext\n```mathematics\nc=d\n```', URL);
+    it('updates the ID of the title of the rendered mathematics', () => {
+      const rendered = render('$$\na=b\n$$', URL);
 
-    const { window: { document } } = new JSDOM(rendered);
-    const titles = document.querySelectorAll('svg title[id]');
+      const { window: { document } } = new JSDOM(rendered);
+      const titles = document.querySelectorAll('svg title[id]');
 
-    assert.equal(titles.length, 2);
-    assert.equal(titles[0].id, 'equation-1');
-    assert.equal(titles[1].id, 'equation-2');
-  });
+      assert.equal(titles.length, 1);
+      assert.equal(titles[0].id, 'equation-1');
+    });
 
-  it('sets aria-labelledby to the title element ID for each snipped', async () => {
-    const rendered = await render('```mathematics\na=b\n```\ntext\n```mathematics\nc=d\n```', URL);
+    it('increments the title ID to avoid collisions', () => {
+      const rendered = render('$$\na=b\n$$\n\ntext\n\n$$\nc=d\n$$', URL);
+      const { window: { document } } = new JSDOM(rendered);
+      const titles = document.querySelectorAll('svg title[id]');
 
-    const { window: { document } } = new JSDOM(rendered);
-    const svgs = document.querySelectorAll('svg');
+      assert.equal(titles.length, 2);
+      assert.equal(titles[0].id, 'equation-1');
+      assert.equal(titles[1].id, 'equation-2');
+    });
 
-    assert.equal(svgs[0].getAttribute('aria-labelledby'), 'equation-1');
-    assert.equal(svgs[1].getAttribute('aria-labelledby'), 'equation-2');
+    it('sets aria-labelledby to the title element ID for each snipped', () => {
+      const rendered = render('$$\na=b\n$$\n\ntext\n\n$$\nc=d\n$$', URL);
+
+      const { window: { document } } = new JSDOM(rendered);
+      const svgs = document.querySelectorAll('svg');
+
+      assert.equal(svgs[0].getAttribute('aria-labelledby'), 'equation-1');
+      assert.equal(svgs[1].getAttribute('aria-labelledby'), 'equation-2');
+    });
   });
 });
