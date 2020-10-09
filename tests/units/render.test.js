f3750b34aaeae340833b57248e6cd783e182bdfe
Mark S. Everitt <mark.s.everitt@gmail.com>
2020-10-09 23:23:27 +0100

    Fixes tests.

--- tests/units/render.test.js
+++ tests/units/render.test.js
@@ -17,7 +17,7 @@ describe('render', () => {
   });
 
   it('renders $$ delimited blocks labelled as SVG in the img role and a title containing the original maths', () => {
-    const rendered = render('\\[\na=b\n\\]', URL);
+    const rendered = render('$$\na=b\n$$', URL);
 
     const { window: { document } } = new JSDOM(rendered);
     const $svg = document.querySelector('svg');
@@ -27,46 +27,6 @@ describe('render', () => {
     const $title = $svg.querySelector('title');
 
     assert.equal($title.textContent, 'a=b');
-  });
-
-  describe('mathematics blocks with brace delimeters', () => {
-    it('removes inline style from rendered mathematics', () => {
-      const rendered = render('\\[\na=b\n\\]', URL);
-
-      const { window: { document } } = new JSDOM(rendered);
-
-      assert.equal(document.querySelectorAll('svg[style]').length, 0);
-    });
-
-    it('updates the ID of the title of the rendered mathematics', () => {
-      const rendered = render('\\[\na=b\n\\]', URL);
-
-      const { window: { document } } = new JSDOM(rendered);
-      const titles = document.querySelectorAll('svg title[id]');
-
-      assert.equal(titles.length, 1);
-      assert.equal(titles[0].id, 'equation-1');
-    });
-
-    it('increments the title ID to avoid collisions', () => {
-      const rendered = render('\\[\na=b\n\\]\n\ntext\n\n\\[\nc=d\n\\]', URL);
-      const { window: { document } } = new JSDOM(rendered);
-      const titles = document.querySelectorAll('svg title[id]');
-
-      assert.equal(titles.length, 2);
-      assert.equal(titles[0].id, 'equation-1');
-      assert.equal(titles[1].id, 'equation-2');
-    });
-
-    it('sets aria-labelledby to the title element ID for each snipped', () => {
-      const rendered = render('\\[\na=b\n\\]\n\ntext\n\n\\[\nc=d\n\\]', URL);
-
-      const { window: { document } } = new JSDOM(rendered);
-      const svgs = document.querySelectorAll('svg');
-
-      assert.equal(svgs[0].getAttribute('aria-labelledby'), 'equation-1');
-      assert.equal(svgs[1].getAttribute('aria-labelledby'), 'equation-2');
-    });
   });
 
   describe('mathematics with dollar delimiters', () => {
