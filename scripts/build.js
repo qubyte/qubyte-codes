1640459cf631987824d8cfaac397379f5cce0e4b
Mark S. Everitt <mark.s.everitt@gmail.com>
2021-08-05 16:18:50 +0100

    Update build script to handle URLs properly for previews.

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,6 @@
 import { build } from '../index.js';
 
-const baseUrl = process.env.URL;
+const baseUrl = process.env.CONTEXT !== 'production' ? process.env.DEPLOY_URL : process.env.URL;
 const baseTitle = process.env.BASE_TITLE;
 const syndications = {
   mastodon: process.env.MASTODON_SYNDICATION,
