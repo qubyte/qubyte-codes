eea9d1bcc3d4fa1d078044cd708ed338f047038a
Mark S. Everitt <mark.s.everitt@gmail.com>
2022-01-30 11:44:26 +0000

    Fix image conversion.

--- functions/function-helpers/upload-image.js
+++ functions/function-helpers/upload-image.js
@@ -2,15 +2,25 @@ import sharp from 'sharp';
 import { upload } from './upload.js';
 
 export async function uploadImage(photo) {
+  console.log('Converting image...', photo);
+
   const time = Date.now();
-  const resized = sharp(photo.content).resize(800);
-  const [jpeg, avif, webp] = Promise.all([
-    resized.jpeg().toBuffer(),
-    resized.avif().toBuffer(),
-    resized.webp().toBuffer()
-  ]);
+  const jpeg = await sharp(photo.content)
+    .resize(800)
+    .jpeg()
+    .toBuffer();
   await upload('New photo (jpeg).\n\n[skip ci]', 'images', `${time}.jpeg`, jpeg);
+
+  const avif = await sharp(photo.content)
+    .resize(800)
+    .avif()
+    .toBuffer();
   await upload('New photo (avif).\n\n[skip ci]', 'images', `${time}.avif`, avif);
+
+  const webp = await sharp(photo.content)
+    .resize(800)
+    .webp()
+    .toBuffer();
   await upload('New photo (webp).\n\n[skip ci]', 'images', `${time}.webp`, webp);
 
   return `/images/${time}.jpeg`;
