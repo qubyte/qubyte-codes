3772095abe4803d1513a64dc0636a34879c5ad40
Mark S. Everitt <mark.s.everitt@gmail.com>
2022-06-09 10:59:29 +0100

    Update image copier to use fs paths rather than URLs.

--- index.js
+++ index.js
@@ -368,10 +368,10 @@ export async function build({ baseUrl, baseTitle, dev, syndications }) {
 
         return new Map(await Promise.all(
           items.map(async item => {
-            const image = sharp(new URL(item, directory));
+            const image = sharp(fileURLToPath(new URL(item, directory)));
             const { width, height } = await image.metadata();
 
-            await image.toFile(new URL(item, imagesTarget));
+            await image.toFile(fileURLToPath(new URL(item, imagesTarget)));
 
             return [`/images/${item}`, { width, height }];
           })
