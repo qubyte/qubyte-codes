dd428997bd43b4d9b3f8971cafc81e7ce623f153
Mark S. Everitt <mark.s.everitt@gmail.com>
2022-10-15 14:23:51 +0100

    Add support for conversion of uploaded images to 2x versions, and their display in notes.

--- functions/function-helpers/upload-image.js
+++ functions/function-helpers/upload-image.js
@@ -12,12 +12,26 @@ export async function uploadImage(photo) {
     .toBuffer();
   await upload('New photo (jpeg).\n\n[skip ci]', 'images', `${time}.jpeg`, jpeg);
 
+  const avif2x = await sharp(photo.content)
+    .rotate()
+    .resize(1600)
+    .avif()
+    .toBuffer();
+  await upload('New photo (avif).\n\n[skip ci]', 'images', `${time}-2x.avif`, avif2x);
+
   const avif = await sharp(photo.content)
     .rotate()
     .resize(800)
     .avif()
     .toBuffer();
   await upload('New photo (avif).\n\n[skip ci]', 'images', `${time}.avif`, avif);
+
+  const webp2x = await sharp(photo.content)
+    .rotate()
+    .resize(1600)
+    .webp()
+    .toBuffer();
+  await upload('New photo (webp).\n\n[skip ci]', 'images', `${time}-2x.webp`, webp2x);
 
   const webp = await sharp(photo.content)
     .rotate()
