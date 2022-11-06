a1ec0c688df5bcf3efd363a5d1505e2e34271a7b
Mark S. Everitt <mark.s.everitt@gmail.com>
2022-11-06 10:58:05 +0000

    Enable under setting for safari, and render the ruby select box even when JS is disabled.

--- content/scripts/ruby-options.js
+++ content/scripts/ruby-options.js
@@ -1,29 +1,4 @@
-const select = document.createElement('select');
-select.className = 'furigana-position';
-select.name = 'furigana mode';
-select.onchange = e => localStorage.setItem('ruby-position', e.target.value);
-
-const over = document.createElement('option');
-over.textContent = 'over';
-over.value = 'over';
-select.append(over);
-
-if (CSS.supports('ruby-position', 'under')) {
-  const under = document.createElement('option');
-  under.textContent = 'under';
-  under.value = 'under';
-  select.append(under);
-}
-
-const off = document.createElement('option');
-off.textContent = 'off';
-off.value = 'off';
-select.append(off);
+const select = document.querySelector('.furigana-position');
 
 select.value = localStorage.getItem('ruby-position') || 'over';
-
-const label = document.createElement('span');
-label.lang = 'ja';
-label.textContent = 'ふりがな';
-
-document.querySelector('main time').after(' ', label, ': ', select);
+select.onchange = e => localStorage.setItem('ruby-position', e.target.value);
