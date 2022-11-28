73fc498050adc12ab21ba12f986f8dfe0ee14843
Mark S. Everitt <mark.s.everitt@gmail.com>
2022-11-28 22:04:58 +0000

    Moves the CSP header to HTML. Fewer moving parts.

--- netlify/edge-functions/add-html-security-headers.js
+++ netlify/edge-functions/add-html-security-headers.js
@@ -1,5 +1,3 @@
-const scriptSrcHashRegex = /^\s*<meta name="script-src-hash" content="(.*)">$/m;
-
 /* eslint-env browser */
 export default async function addHtmlSecurityHeaders(_, context) {
   /** @type Response */
@@ -8,25 +6,17 @@ export default async function addHtmlSecurityHeaders(_, context) {
   const type = headers.get('content-type');
 
   if (!type || type.startsWith('text/html')) {
-    const body = await response.text();
-    const match = body.match(scriptSrcHashRegex);
-
     headers.set('strict-transport-security', 'max-age=31536000; includeSubDomains; preload');
     headers.set('x-frame-options', 'SAMEORIGIN');
     headers.set('x-xss-protection', '1');
     headers.set('x-content-type-options', 'nosniff');
-    headers.set(
-      'content-security-policy',
-      `default-src 'self'; script-src${match ? ` '${match[1]}'` : ''} 'self'; style-src 'self'; img-src *; child-src https://www.youtube-nocookie.com 'self'; frame-src https://www.youtube-nocookie.com 'self';` // eslint-disable-line max-len
-    );
     headers.set('referrer-policy', 'strict-origin-when-cross-origin');
     headers.set('cache-control', 'no-cache');
+    headers.set('content-security-policy', 'default-src \'self\'; img-src *;');
     headers.set(
       'permissions-policy',
       'accelerometer=(self), ambient-light-sensor=(self), camera=(self), fullscreen=(self), gyroscope=(self), magnetometer=(self), microphone=(self), midi=(self), picture-in-picture=(), sync-xhr=(), usb=(self), interest-cohort=()' // eslint-disable-line max-len
     );
-
-    return new Response(body, response);
   }
 
   return response;
