d32a91c983123b7eea8d9997b1f60a4fa8eeb292
Mark S. Everitt <mark.s.everitt@gmail.com>
2022-12-26 01:23:17 +0000

    Further performance enhancements to the media endpoint.

--- functions/function-helpers/upload-image.js
+++ functions/function-helpers/upload-image.js
@@ -1,29 +1,28 @@
 import sharp from 'sharp';
 import { upload } from './upload.js';
+import PQueue from 'p-queue';
 
 export async function uploadImage(photo) {
   console.log(new Date().toISOString(), 'Converting image...', photo);
 
+  // GitHub gets upset with concurrent use of the content API.
+  const queue = new PQueue({ concurrency: 1 });
   const time = Date.now();
   const s = sharp(photo.content, { sequentialRead: true }).rotate();
 
-  function convertTo(width, format) {
-    return s.clone()
+  function convertTo(width, format, name) {
+    s.clone()
       .resize(width)
-      .toFormat(format)
-      .toBuffer();
+      .toFormat(format, { effort: 3 })
+      .toBuffer()
+      .then(buffer => queue.add(() => upload(`New photo (${name}).\n\n[skip ci]`, 'images', name, buffer)));
   }
 
-  const [jpeg, avif2x, avif] = await Promise.all([
-    convertTo(800, 'jpeg'),
-    convertTo(1600, 'avif'),
-    convertTo(800, 'avif')
+  await Promise.all([
+    convertTo(800, 'jpeg', `${time}.jpeg`),
+    convertTo(1600, 'avif', `${time}-2x.avif`),
+    convertTo(800, 'avif', `${time}.avif`)
   ]);
-
-  // I can't do these concurrently without upsetting GitHub.
-  await upload('New photo (jpeg).\n\n[skip ci]', 'images', `${time}.jpeg`, jpeg);
-  await upload('New photo (avif 2x).\n\n[skip ci]', 'images', `${time}-2x.avif`, avif2x);
-  await upload('New photo (avif).\n\n[skip ci]', 'images', `${time}.avif`, avif);
 
   return `/images/${time}.jpeg`;
 }
