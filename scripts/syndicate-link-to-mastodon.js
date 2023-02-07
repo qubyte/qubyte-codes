03ca58aa97d63d9f1860182c3631e1d8d3bbf7f1
Mark S. Everitt <mark.s.everitt@gmail.com>
2023-02-07 11:21:17 +0000

    Updates syndication scripts to process more than one entry.

--- scripts/syndicate-link-to-mastodon.js
+++ scripts/syndicate-link-to-mastodon.js
@@ -1,17 +1,19 @@
 import { readFile } from 'node:fs/promises';
+import { parseArgs } from 'node:util';
 import postToMastodon from '../lib/post-to-mastodon.js';
 
-const path = process.argv[2];
+const { positionals: paths } = parseArgs({ allowPositionals: true });
 
-if (!path) {
+if (!paths.length) {
   console.log('No path given.'); // eslint-disable-line no-console
-  process.exit(0);
 }
 
-console.log('Processing:', path); // eslint-disable-line no-console
+for (const path of paths) {
+  console.log('Processing:', paths); // eslint-disable-line no-console
 
-const link = await readFile(path, 'utf8');
-const { properties: { content: [content], name: [name], 'bookmark-of': [url] } } = JSON.parse(link);
-const statusBody = new URLSearchParams({ status: `${content}\n\n${name}: ${url}`.trim() });
+  const link = await readFile(path, 'utf8');
+  const { properties: { content: [content], name: [name], 'bookmark-of': [url] } } = JSON.parse(link);
+  const statusBody = new URLSearchParams({ status: `${content}\n\n${name}: ${url}`.trim() });
 
-await postToMastodon('/api/v1/statuses', statusBody);
+  await postToMastodon('/api/v1/statuses', statusBody);
+}
