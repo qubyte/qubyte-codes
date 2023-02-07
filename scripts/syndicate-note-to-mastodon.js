3f5cc692a60c17e2a36045ffc2f437241a0a54e7
Mark S. Everitt <mark.s.everitt@gmail.com>
2023-02-07 11:21:17 +0000

    Updates syndication scripts to process more than one entry.

--- scripts/syndicate-note-to-mastodon.js
+++ scripts/syndicate-note-to-mastodon.js
@@ -1,31 +1,33 @@
 /* global FormData, Blob */
 
 import { readFile } from 'node:fs/promises';
+import { parseArgs } from 'node:util';
 import postToMastodon from '../lib/post-to-mastodon.js';
 
-const path = process.argv[2];
+const { positionals: paths } = parseArgs({ allowPositionals: true });
 
-if (!path) {
+if (!paths.length) {
   console.log('No path given.'); // eslint-disable-line no-console
-  process.exit(0);
 }
 
-console.log('Processing:', path); // eslint-disable-line no-console
+for (const path of paths) {
+  console.log('Processing:', path); // eslint-disable-line no-console
 
-const { properties: { content: [status], photo = [] } } = JSON.parse(await readFile(path, 'utf8'));
-const statusBody = new URLSearchParams({ status });
+  const { properties: { content: [status], photo = [] } } = JSON.parse(await readFile(path, 'utf8'));
+  const statusBody = new URLSearchParams({ status });
 
-for (const { value, alt } of photo) {
-  const form = new FormData();
-  const filePath = `content/${value}`.replace('.jpeg', '-original.jpeg');
-  const content = await readFile(filePath);
+  for (const { value, alt } of photo) {
+    const form = new FormData();
+    const filePath = `content/${value}`.replace('.jpeg', '-original.jpeg');
+    const content = await readFile(filePath);
 
-  form.set('file', new Blob([content], { type: 'image/jpeg' }), 'image.jpeg');
-  form.set('description', alt);
+    form.set('file', new Blob([content], { type: 'image/jpeg' }), 'image.jpeg');
+    form.set('description', alt);
 
-  const { id } = await postToMastodon('/api/v2/media', form);
+    const { id } = await postToMastodon('/api/v2/media', form);
 
-  statusBody.append('media_ids[]', id);
+    statusBody.append('media_ids[]', id);
+  }
+
+  await postToMastodon('/api/v1/statuses', statusBody);
 }
-
-await postToMastodon('/api/v1/statuses', statusBody);
