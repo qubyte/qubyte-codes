cfa758186e65c283f51088a5c85a5c14244ada95
Mark S. Everitt <mark.s.everitt@gmail.com>
2023-08-24 18:46:23 +0100

    Update add-html-security-headers.js

--- netlify/edge-functions/add-html-security-headers.js
+++ netlify/edge-functions/add-html-security-headers.js
@@ -8,24 +8,28 @@ export default async function addHtmlSecurityHeaders(request, context) {
   const { headers } = response;
   const type = headers.get('content-type');
 
-  if (!type || type.startsWith('text/html')) {
-    const body = await response.text();
-    const match = body.match(scriptSrcHashRegex);
-
-    headers.set('strict-transport-security', 'max-age=31536000; includeSubDomains; preload');
-    headers.set('x-frame-options', 'SAMEORIGIN');
-    headers.set('x-xss-protection', '1');
-    headers.set('x-content-type-options', 'nosniff');
-    headers.set(
-      'content-security-policy',
-      `default-src 'self'; script-src${match ? ` '${match[1]}'` : ''} 'self'; style-src 'self'; img-src *; child-src https://www.youtube-nocookie.com 'self'; frame-src https://www.youtube-nocookie.com 'self';` // eslint-disable-line max-len
-    );
-    headers.set('referrer-policy', 'strict-origin-when-cross-origin');
-    headers.set('cache-control', 'max-age=0, s-maxage=31536000');
-    headers.set(
-      'permissions-policy',
-      'accelerometer=(self), ambient-light-sensor=(self), camera=(self), fullscreen=(self), gyroscope=(self), magnetometer=(self), microphone=(self), midi=(self), picture-in-picture=(), sync-xhr=(), usb=(self), interest-cohort=()' // eslint-disable-line max-len
-    );
+  try {
+    if (!type || type.startsWith('text/html')) {
+      const body = await response.text();
+      const match = body.match(scriptSrcHashRegex);
+  
+      headers.set('strict-transport-security', 'max-age=31536000; includeSubDomains; preload');
+      headers.set('x-frame-options', 'SAMEORIGIN');
+      headers.set('x-xss-protection', '1');
+      headers.set('x-content-type-options', 'nosniff');
+      headers.set(
+        'content-security-policy',
+        `default-src 'self'; script-src${match ? ` '${match[1]}'` : ''} 'self'; style-src 'self'; img-src *; child-src https://www.youtube-nocookie.com 'self'; frame-src https://www.youtube-nocookie.com 'self';` // eslint-disable-line max-len
+      );
+      headers.set('referrer-policy', 'strict-origin-when-cross-origin');
+      headers.set('cache-control', 'max-age=0, s-maxage=31536000');
+      headers.set(
+        'permissions-policy',
+        'accelerometer=(self), ambient-light-sensor=(self), camera=(self), fullscreen=(self), gyroscope=(self), magnetometer=(self), microphone=(self), midi=(self), picture-in-picture=(), sync-xhr=(), usb=(self), interest-cohort=()' // eslint-disable-line max-len
+      );
+    } catch (e) {
+      console.error("Unexpected error:", response.status, e);
+    }
 
     return new Response(body, response);
   }
