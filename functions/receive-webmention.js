3576e3e32ebbe3613688da675c574d8e4e64eb5e
Mark S. Everitt <mark.s.everitt@gmail.com>
2024-03-14 18:45:26 +0000

    Fixes webmentions for the colophon for real, again.

--- functions/receive-webmention.js
+++ functions/receive-webmention.js
@@ -85,7 +85,10 @@ async function checkSource(source, target) {
     return false;
   }
 
-  const contentType = res.headers.get('content-type') || 'text/html';
+  const contentTypeHeader = res.headers.get('content-type') || 'text/html';
+  const contentType = contentTypeHeader
+    .split(';')[0]
+    .trim();
 
   if (contentType !== 'text/html' && contentType !== 'application/xhtml+xml') {
     return false;
